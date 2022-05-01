ef00abdf70a7ad63ab3fcfdf210e88b1e0561c50
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-01 22:32:11 +0000

    Article Service

--- src/main/java/com/springboot/blog/service/ArticleService.java
+++ src/main/java/com/springboot/blog/service/ArticleService.java
@@ -1,7 +1,10 @@
 package com.springboot.blog.service;
 
+import com.springboot.blog.repo.ArticleRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ArticleService {
+
+    private ArticleRepository articleRepository;
 }
