da4c0c1f5bc5226bc0229142680e42907020205e
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-01 19:56:36 +0000

    Article class Updated

--- src/main/java/com/springboot/blog/model/Article.java
+++ src/main/java/com/springboot/blog/model/Article.java
@@ -5,6 +5,8 @@ import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
+import java.util.Date;
+
 @Document(indexName = "ARTICLE_DOCUMENT",type = "article")
 public class Article {
 
@@ -21,4 +23,7 @@ public class Article {
 
     @Field(type = FieldType.Nested)
     private User author;
+
+    @Field(type = FieldType.Date)
+    private Date createdDate = new Date();
 }
