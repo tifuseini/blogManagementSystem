559c91a0502b4388e8d275046d302524a48c07bf
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-01 22:29:22 +0000

    articile controller

--- src/main/java/com/springboot/blog/controller/ArticleController.java
+++ src/main/java/com/springboot/blog/controller/ArticleController.java
@@ -1,7 +1,9 @@
 package com.springboot.blog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/article")
 public class ArticleController {
 }
