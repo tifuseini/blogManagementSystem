e58c6227d644bef774bbff9c6726cfc56a36840f
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-01 22:25:35 +0000

    Index Controller update

--- src/main/java/com/springboot/blog/controller/Indexcontroller.java
+++ src/main/java/com/springboot/blog/controller/Indexcontroller.java
@@ -1,4 +1,13 @@
 package com.springboot.blog.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class Indexcontroller {
+
+    @GetMapping("/")
+    public String index(){
+        return "redirect:/article";
+    }
 }
