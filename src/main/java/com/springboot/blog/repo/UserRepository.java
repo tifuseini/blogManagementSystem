4c0a0a1655d50e15fde32c15b4e0d4a3d4eac9d8
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-01 20:31:59 +0000

    User Repository Clas Update

--- src/main/java/com/springboot/blog/repo/UserRepository.java
+++ src/main/java/com/springboot/blog/repo/UserRepository.java
@@ -4,5 +4,5 @@ import com.springboot.blog.model.User;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface UserRepository extends ElasticsearchRepository<User,String> {
-    User findByUsername(String username)
+    User findByUsername(String username);
 }
