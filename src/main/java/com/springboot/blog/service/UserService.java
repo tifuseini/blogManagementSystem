40a89513c677f6d485236231ab33213518076044
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-01 22:57:04 +0000

    User Service

--- src/main/java/com/springboot/blog/service/UserService.java
+++ src/main/java/com/springboot/blog/service/UserService.java
@@ -9,24 +9,21 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository){
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    public User save(User user){
+    public User save(User user) {
         return userRepository.save(user);
     }
 
-    public User getByUsername(String username){
+    public User getByUsername(String username) {
         return userRepository.findByUsername(username);
     }
 
-    public void deleteAll(){
+    public void deleteAll() {
         userRepository.deleteAll();
     }
-
-
-
 
 
 }
