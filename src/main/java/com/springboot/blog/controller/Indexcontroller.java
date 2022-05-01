63fc15f1bb4ccebc919e6949bd4b32edac3c2dd5
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-01 22:25:46 +0000

    Index Controller update

--- src/main/java/com/springboot/blog/controller/Indexcontroller.java
+++ src/main/java/com/springboot/blog/controller/Indexcontroller.java
@@ -3,6 +3,9 @@ package com.springboot.blog.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+
+
 @Controller
 public class Indexcontroller {
 
