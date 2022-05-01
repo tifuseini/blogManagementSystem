452587faccb2dbcf8194d615d085532011e05473
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-01 20:39:51 +0000

    Article Repository Update

--- src/main/java/com/springboot/blog/repo/ArticleRepository.java
+++ src/main/java/com/springboot/blog/repo/ArticleRepository.java
@@ -11,7 +11,5 @@ public interface ArticleRepository extends ElasticsearchRepository<Article,Strin
 
     Optional<Article> findByLink(String link);
 
-
-
     Page<Article> findByTitleContainingAndBodyContaining(String title, String body, Pageable pageable);
 }
