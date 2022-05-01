e8950c23eb90647d4f14925284060154e67795af
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-01 23:10:19 +0000

    Article Controller

--- src/main/java/com/springboot/blog/controller/ArticleController.java
+++ src/main/java/com/springboot/blog/controller/ArticleController.java
@@ -5,6 +5,7 @@ import com.springboot.blog.service.UserService;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
