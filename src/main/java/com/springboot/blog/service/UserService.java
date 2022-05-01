9778f4d8e3d544d23354a06ee9fb5f2eb16bed07
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-01 22:51:26 +0000

    User Service

--- src/main/java/com/springboot/blog/service/UserService.java
+++ src/main/java/com/springboot/blog/service/UserService.java
@@ -1,7 +1,10 @@
 package com.springboot.blog.service;
 
+import com.springboot.blog.repo.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
 }
