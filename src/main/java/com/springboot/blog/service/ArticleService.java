4b2406baa363269998645813c6fe693856ef9f7d
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-01 22:37:26 +0000

    Article Service

--- src/main/java/com/springboot/blog/service/ArticleService.java
+++ src/main/java/com/springboot/blog/service/ArticleService.java
@@ -1,7 +1,10 @@
 package com.springboot.blog.service;
 
+import com.springboot.blog.model.Article;
 import com.springboot.blog.repo.ArticleRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class ArticleService {
@@ -11,4 +14,12 @@ public class ArticleService {
     public ArticleService(ArticleRepository articleRepository){
         this.articleRepository = articleRepository;
     }
+
+    public Article save(Article article){
+        if(article.getId() == null){
+            article.setId(UUID.randomUUID().toString());
+        }
+        re
+    }
+
 }
