5d5a56e00d8dfd5a7aeef8443891aabbd99ee545
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-01 23:12:36 +0000

    Article Controller

--- src/main/java/com/springboot/blog/controller/ArticleController.java
+++ src/main/java/com/springboot/blog/controller/ArticleController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Optional;
+
 @Controller
 @RequestMapping("/article")
 public class ArticleController {
