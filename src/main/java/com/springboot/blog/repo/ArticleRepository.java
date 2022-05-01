4bcf0c8ba17a34b3803134a1a141b9e90411594e
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-01 20:37:05 +0000

    Article Repository Update

--- src/main/java/com/springboot/blog/repo/ArticleRepository.java
+++ src/main/java/com/springboot/blog/repo/ArticleRepository.java
@@ -2,6 +2,7 @@ package com.springboot.blog.repo;
 
 import com.springboot.blog.model.Article;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import java.util.Optional;
@@ -10,5 +11,5 @@ public interface ArticleRepository extends ElasticsearchRepository<Article,Strin
 
     Optional<Article> findByLink(String link);
 
-    Page<Article> findByTitleAndBody(String title,String body.Pageable pageable)
+    Page<Article> findByTitleContainingAndBodyContaining(String title, String body, Pageable pageable);
 }
