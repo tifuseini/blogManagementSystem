783f1f9fba168a0516db7d68f13dcca437a656da
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-01 22:34:33 +0000

    Article repo

--- src/main/java/com/springboot/blog/controller/ArticleController.java
+++ src/main/java/com/springboot/blog/controller/ArticleController.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/article")
 public class ArticleController {
 
-    private final ArticleService articleService;
+//    private final ArticleService articleService;
 }
