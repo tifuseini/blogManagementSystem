f6f1bf0f2ca0b355a4e18aed5a3fd345686238fb
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-01 20:01:26 +0000

    User Class Update

--- src/main/java/com/springboot/blog/model/User.java
+++ src/main/java/com/springboot/blog/model/User.java
@@ -1,7 +1,13 @@
 package com.springboot.blog.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.elasticsearch.annotations.Document;
 
 @Document(indexName = "USER",type = "user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 }
