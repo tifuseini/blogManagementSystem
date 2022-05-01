5f14fff765ff3228c11cf5c0c5f80e7e622e2617
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-01 19:51:23 +0000

    Article class Updated

--- src/main/java/com/springboot/blog/model/Article.java
+++ src/main/java/com/springboot/blog/model/Article.java
@@ -1,7 +1,15 @@
 package com.springboot.blog.model;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 
 @Document(indexName = "ARTICLE_DOCUMENT")
 public class Article {
+
+    @Id
+    private String id;
+
+
+
+
 }
