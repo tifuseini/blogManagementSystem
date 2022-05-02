e3636e6ee77829c210341474dee74e06d4351703
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-02 19:24:48 +0000

    Security Config

--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -35,6 +36,6 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetails
+    public UserDetailsRepositoryReactiveAuthenticationManager
 
 }
