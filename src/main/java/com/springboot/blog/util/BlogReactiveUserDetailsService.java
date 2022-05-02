ab38f554449c7ce5506c3a2a4071d5947b397762
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-02 19:52:20 +0000

    Blog Reactive User Details Service

--- src/main/java/com/springboot/blog/util/BlogReactiveUserDetailsService.java
+++ src/main/java/com/springboot/blog/util/BlogReactiveUserDetailsService.java
@@ -18,6 +18,7 @@ public class BlogReactiveUserDetailsService implements ReactiveUserDetailsServic
 
     @Override
     public Mono<UserDetails> findByUsername(String s) {
+        User user - userRepository.findByUsername(s)
 
     }
 }
