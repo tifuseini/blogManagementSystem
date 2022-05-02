cb69e2524626200095ec759c60bc69014ed07116
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-02 18:36:18 +0000

    Base Controller Advice Update

--- src/main/java/com/springboot/blog/controller/BaseControllerAdvice.java
+++ src/main/java/com/springboot/blog/controller/BaseControllerAdvice.java
@@ -1,7 +1,10 @@
 package com.springboot.blog.controller;
 
+import com.springboot.blog.service.UserService;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice
 public class BaseControllerAdvice {
+
+    private final UserService userService;
 }
