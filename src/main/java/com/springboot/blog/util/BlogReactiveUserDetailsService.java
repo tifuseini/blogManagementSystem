c71e270a2b45d54019f0e1a92075f142350117a8
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-02 19:48:29 +0000

    Blog Reactive User Details Service

--- src/main/java/com/springboot/blog/util/BlogReactiveUserDetailsService.java
+++ src/main/java/com/springboot/blog/util/BlogReactiveUserDetailsService.java
@@ -1,8 +1,11 @@
 package com.springboot.blog.util;
 
+import com.springboot.blog.repo.UserRepository;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BlogReactiveUserDetailsService implements ReactiveUserDetailsService {
+
+    private final UserRepository userRepository;
 }
