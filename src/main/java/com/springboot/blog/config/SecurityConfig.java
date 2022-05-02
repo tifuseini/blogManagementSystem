c1b7ab44dff79c5f80e2f09eb909edc27b0fc095
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-02 19:31:38 +0000

    Security Config

--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
@@ -46,7 +47,7 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return new B
+        return new BCryptPasswordEncoder()
     }
 
 
