3479758d6b87ed26d9f0e8fabd6e0f39cd913cdf
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-02 20:29:23 +0000

    Config

--- src/main/java/com/springboot/blog/config/Config.java
+++ src/main/java/com/springboot/blog/config/Config.java
@@ -3,6 +3,7 @@ package com.springboot.blog.config;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
@@ -11,6 +12,7 @@ public class Config {
 
     @Bean
     public RestHighLevelClient client(){
+        ClientConfiguration clientConfiguration
 
     }
 }
