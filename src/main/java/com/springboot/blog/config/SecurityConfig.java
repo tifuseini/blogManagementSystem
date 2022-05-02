5e1752a2d5f545429bcba62b1d524ddad908b1fe
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-02 19:10:53 +0000

    Security Config

--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.springboot.blog.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
 }
