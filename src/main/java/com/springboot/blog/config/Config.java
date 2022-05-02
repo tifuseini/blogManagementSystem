9119e599a7c9539448c032938d84e72b6b3f1d99
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-02 20:59:37 +0000

    Not Working

--- src/main/java/com/springboot/blog/config/Config.java
+++ src/main/java/com/springboot/blog/config/Config.java
@@ -3,14 +3,17 @@ package com.springboot.blog.config;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.springboot.blog.repo")
+@ComponentScan(basePackages = { "com.springboot.blog" })
 public class Config {
 
     @Bean
@@ -18,7 +21,7 @@ public class Config {
         ClientConfiguration clientConfiguration =
                 ClientConfiguration.builder()
                         .connectedTo("localhost:9200").build();
-        return RestClient.create(clientConfiguration).rest();
+        return RestClients.create(clientConfiguration).rest();
 
     }
 
