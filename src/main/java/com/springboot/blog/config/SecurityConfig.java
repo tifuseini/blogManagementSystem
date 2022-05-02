ce62fcc9af49bb0f0c10ba6c82b9f2780d13ed5b
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-02 19:40:03 +0000

    Security Config

--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -60,6 +60,7 @@ public class SecurityConfig {
             userService.deleteAll();
             userService.save( new User(UUID.randomUUID().toString(),"user",passwordEncoder()
                     .encode("password"),"USER","User of the blog"));
+            userService.save()
 
         };
 
