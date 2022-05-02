a13ca9647e12df1d6ee43e6f63d9277e79d22112
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-02 19:51:17 +0000

    Blog Reactive User Details Service

--- src/main/java/com/springboot/blog/util/BlogReactiveUserDetailsService.java
+++ src/main/java/com/springboot/blog/util/BlogReactiveUserDetailsService.java
@@ -2,7 +2,9 @@ package com.springboot.blog.util;
 
 import com.springboot.blog.repo.UserRepository;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Service
 public class BlogReactiveUserDetailsService implements ReactiveUserDetailsService {
@@ -10,6 +12,12 @@ public class BlogReactiveUserDetailsService implements ReactiveUserDetailsServic
     private final UserRepository userRepository;
 
     public BlogReactiveUserDetailsService(UserRepository userRepository){
+        this.userRepository = userRepository;
 
+    }
+
+    @Override
+    public Mono<UserDetails> findByUsername(String username) {
+        return null;
     }
 }
