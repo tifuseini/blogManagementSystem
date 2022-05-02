ea4210cfeaf45e2c1ecd0f5421fbe78333bf934d
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-02 18:38:36 +0000

    Base Controller Advice Update

--- src/main/java/com/springboot/blog/controller/BaseControllerAdvice.java
+++ src/main/java/com/springboot/blog/controller/BaseControllerAdvice.java
@@ -2,6 +2,8 @@ package com.springboot.blog.controller;
 
 import com.springboot.blog.service.UserService;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.webjars.NotFoundException;
 
 @ControllerAdvice
 public class BaseControllerAdvice {
@@ -11,4 +13,7 @@ public class BaseControllerAdvice {
     public BaseControllerAdvice(UserService userService){
         this.userService = userService;
     }
+
+    @ExceptionHandler(NotFoundException.class)
+
 }
