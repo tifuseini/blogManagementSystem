2947c14f07289aa4c622d693deeb0206271e06e6
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-02 19:13:45 +0000

    Security Config

--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -18,5 +18,5 @@ public class SecurityConfig {
     private UserService userService;
 
     @Bean
-    public SecurityWebFilterChain(ServerHttpSecurity)
+    public SecurityWebFilterChain(ServerHttpSecurity http)
 }
