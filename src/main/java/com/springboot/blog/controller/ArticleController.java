6dd2f6e9b49ca4acd83ea5853431a53be1f55104
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-02 12:44:28 +0000

    Article Controller

--- src/main/java/com/springboot/blog/controller/ArticleController.java
+++ src/main/java/com/springboot/blog/controller/ArticleController.java
@@ -10,10 +10,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.webjars.NotFoundException;
 
 import java.util.Optional;
