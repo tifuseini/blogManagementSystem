81ce19b4408437d54581f2065f132d94c0d78c68
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-02 20:26:49 +0000

    Config

--- src/main/java/com/springboot/blog/config/Config.java
+++ src/main/java/com/springboot/blog/config/Config.java
@@ -1,7 +1,9 @@
 package com.springboot.blog.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
+@EnableElasticsearchRepositories
 public class Config {
 }
