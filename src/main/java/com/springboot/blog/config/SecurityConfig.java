c29788242203d7a8396b124a3932175f0a63f5ec
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-02 19:12:16 +0000

    Security Config

--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.springboot.blog.config;
 
 import com.springboot.blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
@@ -13,4 +14,6 @@ public class SecurityConfig {
 
     @Autowired
     private UserService userService;
+
+    @Bean
 }
