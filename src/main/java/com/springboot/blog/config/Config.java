c6b73d75f7b41af4bdd050b7f796c4880370e26e
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-02 20:30:51 +0000

    Config

--- src/main/java/com/springboot/blog/config/Config.java
+++ src/main/java/com/springboot/blog/config/Config.java
@@ -14,7 +14,8 @@ public class Config {
     public RestHighLevelClient client(){
         ClientConfiguration clientConfiguration =
                 ClientConfiguration.builder()
-                        .connectedTo("localhost:9200").build()
+                        .connectedTo("localhost:9200").build();
+        return
 
     }
 }
