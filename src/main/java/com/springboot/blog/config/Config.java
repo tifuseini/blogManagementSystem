c1d274cf078fcee245e3c3b6e3863896de2253a2
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-02 20:31:18 +0000

    Config

--- src/main/java/com/springboot/blog/config/Config.java
+++ src/main/java/com/springboot/blog/config/Config.java
@@ -1,5 +1,6 @@
 package com.springboot.blog.config;
 
+import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +16,7 @@ public class Config {
         ClientConfiguration clientConfiguration =
                 ClientConfiguration.builder()
                         .connectedTo("localhost:9200").build();
-        return
+        return RestClient
 
     }
 }
