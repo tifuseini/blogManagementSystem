5c6e8db3afea90ce509136c447efdca0e6b35cd5
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-02 20:36:28 +0000

    Config

--- src/main/java/com/springboot/blog/config/Config.java
+++ src/main/java/com/springboot/blog/config/Config.java
@@ -5,6 +5,7 @@ import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
